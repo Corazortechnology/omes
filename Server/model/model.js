d3e750567bc9e6240a2360d270ae47b24864e214
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-11-20 18:05:51 +0530

    model and router

--- Server/model/model.js
+++ Server/model/model.js
@@ -1,3 +1,18 @@
+// const mongoose = require("mongoose");
+// var schema = new mongoose.Schema(
+//   {
+//     active: {
+//       type: String,
+//     },
+//     status: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const UserDB = mongoose.model("ome", schema);
+// module.exports = UserDB;
 const mongoose = require("mongoose");
 var schema = new mongoose.Schema(
   {
@@ -6,6 +21,10 @@ var schema = new mongoose.Schema(
     },
     status: {
       type: String,
+    },
+    profession: {
+      type: String,
+      default: "Computer Science",
     },
   },
   { timestamps: true }
